8174e330977168fc2e039659d1e1b181034c6024
Gus (Nala) <gusrosales7@gmail.com>
2020-03-23 09:54:02 -0700

    playing around

--- server.js
+++ server.js
@@ -11,25 +11,27 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(`assets`))
+
 
 
 
 // Basic route that sends the user first to the AJAX Page
 app.get(`/`,(req,res)=>{
-    res.sendFile(path.join(__dirname,`./index.html`))
+    res.sendFile(path.join(__dirname,`./assets/index.html`))
 })
 app.get("/:term", function (req, res) {
     const page = req.params.term
 
     switch (page) {
         case "portfolio":
-            res.sendFile(path.join(__dirname, `./portfolio.html`))
+            res.sendFile(path.join(__dirname, `./assets/portfolio.html`))
             break;
         case "":
-            res.sendFile(path.join(__dirname, `./index.html`))
+            res.sendFile(path.join(__dirname, `./assets/index.html`))
             break;
         default:
-            res.sendFile(path.join(__dirname, `./foOhFo.html`))
+            res.sendFile(path.join(__dirname, `./assets/foOhFo.html`))
             break;
     }
     // res.sendFile(path.join(__dirname, "../index.html"));
