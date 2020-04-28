b8db6b83bdb11a385335e2d955341221a7540dcb
Gus (Nala) <gusrosales7@gmail.com>
2020-04-28 14:02:18 -0700

    added images to portfolio. resized homepage images

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+    let viewportWidth, viewportHeight, footerHeight;
+
+    const changeCSS = () => {
+        viewportWidth = $(window).innerWidth()
+        viewportHeight = $(window).innerHeight()
+        footerHeight = $(`.footer`).outerHeight()
+
+
+        $(`.callout-index`).attr(`style`, `height: ${viewportHeight}px`)			// making the top header "callout" responsive and fill the screen when at top of page
+        $(`.header-index`).attr(`style`, `padding-top: ${(viewportHeight) / 2}px;`)			// making sure the padding to the text within is reponsive as well
+        // $(`#index-last-row`).attr(`style`, `margin-bottom: ${footerHeight}px`)
+    }
+
+    changeCSS()
+
+    $(window).resize(() => {
+
+
+        changeCSS()
+    })
+})
