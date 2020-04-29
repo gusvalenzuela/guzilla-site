4cec9ff9336eb9b01ca534a99921e6a49c910cc1
Gus (Nala) <gusrosales7@gmail.com>
2020-04-29 14:40:30 -0700

    minor css changes to header and sub-header-index

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
     viewportHeight = $(window).innerHeight();
 
     $(`.callout-index`).attr(`style`, `height: ${viewportHeight}px`); // making the top header "callout" responsive and fill the screen when at top of page
-    $(`.header-index`).attr(`style`, `padding-top: ${viewportHeight / 2}px;`); // making sure the padding to the text within is reponsive as well
+    $(`.header-index`).attr(`style`, `padding-top: ${viewportHeight / 1.912345678}px;`); // making sure the padding to the text within is reponsive as well
     // $(`#index-last-row`).attr(`style`, `margin-bottom: ${footerHeight}px`)
   };
 
